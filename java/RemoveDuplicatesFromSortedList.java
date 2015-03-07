2d52f53d917240f0379d0e1e9ad5375909ae9683
rioshen <rioxshen@gmail.com>
2015-03-07 15:02:40 -0500

    Add one more easy level linked list problem.

---
+++ java/RemoveDuplicatesFromSortedList.java
@@ -0,0 +1,20 @@
+public class RemoveDuplicatesFromSortedList {
+    /**
+     * @param ListNode head is the head of the linked list
+     * @return: ListNode head of linked list
+     */
+    public static ListNode deleteDuplicates(ListNode head) {
+        if (head == null || head.next == null) return head;
+
+        ListNode curr = head;
+        while (curr != null && curr.next != null) {
+            if (curr.val == curr.next.val) {
+                curr.next = curr.next.next;
+            } else {
+                curr = curr.next;
+            }
+        }
+
+        return head;
+    }
+}
